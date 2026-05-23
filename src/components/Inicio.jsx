bde2d67b8f6e304086c81b37b581617a4c2c2e26
Rodrigo Gamarra <rodrigo007g987@gmail.com>
2026-05-23 09:57:21 -0400

    Refactor de dead code en pagina inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,38 +1,8 @@
-export default function Inicio({ huespedes, reservas, servicios }) {
-  const reservasEnCurso = reservas.filter((r) => r.estado === "En curso").length;
-
+export default function Inicio() {
   return (
     <div>
       <h1>Gestion de Reservas para hotel</h1>
-      
-
-      <div className="tarjetas-resumen">
-        {/*}
-        <div className="tarjeta">
-          <h3>Huéspedes</h3>
-          <p>{huespedes.length}</p>
-        </div>
-
-        <div className="tarjeta">
-          <h3>Reservas</h3>
-          <p>{reservas.length}</p>
-        </div>
-
-        <div className="tarjeta">
-          <h3>En curso</h3>
-          <p>{reservasEnCurso}</p>
-        </div>
-
-        <div className="tarjeta">
-          <h3>Servicios</h3>
-          <p>{servicios.length}</p>
-        </div>*/}
-
-      </div>
-      {/*<div>
-        <img src="../logos/hotelLogo.jpg"></img>
-      </div>*/}
-
+      <div className="tarjetas-resumen"></div>
     </div>
   );
 }
